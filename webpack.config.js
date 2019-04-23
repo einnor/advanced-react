fe19e4aeee29b4e8a255c341d0599a6ed9315908
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-04-23 17:18:01 +0300

    Separating vendor files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   resolve: {
@@ -12,15 +13,49 @@ const config = {
 
     ],
   },
-  entry: ['@babel/polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      '@babel/polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby',
+    ],
+    app: ['./lib/renderers/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
     ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
   }
 };
 
